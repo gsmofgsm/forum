e9dc391fd8e3cd6ff7276f6a740df4b5a9654a74
vagrant <vagrant@homestead>
2017-03-22 21:13:38 +0000

    Test Reply owner

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,15 +5,29 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">All Threads</div>
+                    <div class="panel-heading">{{ $thread->title }}</div>
 
                     <div class="panel-body">
-                        <article>
-                            <h4>{{ $thread->title }}</h4>
-                            {{ $thread->body }}
-                        </article>
+                        {{ $thread->body }}
                     </div>
                 </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                @foreach( $thread->replies as $reply)
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            {{ $reply->owner->name }} said
+                            {{ $reply->created_at->diffForHumans() }}
+                        </div>
+
+                        <div class="panel-body">
+                            {{ $reply->body }}
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
--- tests/Feature/ThreadTest.php
+++ tests/Feature/ThreadTest.php
@@ -37,6 +37,9 @@ class ExampleTest extends TestCase
     /** @test */
     function a_user_can_read_replies_that_are_associated_with_a_thread()
     {
-        
+        $reply = factory('App\Reply')->create(['thread_id' => $this->thread->id]);
+
+        $this->get('/threads/' . $this->thread->id)
+             ->assertSee($reply->body);
     }
 }
