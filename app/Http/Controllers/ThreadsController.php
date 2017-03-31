ca7e90d3b4872637ef78658da2017436be4aa639
vagrant <vagrant@homestead>
2017-03-31 21:17:17 +0000

    thread.index refactoring - getThread to a method

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -22,18 +22,7 @@ class ThreadsController extends Controller
      */
     public function index(Channel $channel)
     {
-        if($channel->exists){
-            $threads = $channel->threads()->latest();
-        }else {
-            $threads = Thread::latest();
-        }
-
-        if($username = request('by')){
-            $user = User::where('name', $username)->firstOrFail();
-            $threads = $threads->where('user_id', $user->id);
-        }
-
-        $threads = $threads->get();
+        $threads = $this->getThread($channel);
 
         return view('threads.index', compact('threads'));
     }
@@ -115,5 +104,26 @@ class ThreadsController extends Controller
     public function destroy(Thread $thread)
     {
         //
+    }
+
+    /**
+     * @param Channel $channel
+     * @return mixed
+     */
+    protected function getThread(Channel $channel)
+    {
+        if ($channel->exists) {
+            $threads = $channel->threads()->latest();
+        } else {
+            $threads = Thread::latest();
+        }
+
+        if ($username = request('by')) {
+            $user = User::where('name', $username)->firstOrFail();
+            $threads = $threads->where('user_id', $user->id);
+        }
+
+        $threads = $threads->get();
+        return $threads;
     }
 }
