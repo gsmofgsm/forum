473d130efb9ae29f44f42aeb0bb2dbc98020ea0c
vagrant <vagrant@homestead>
2017-03-31 21:43:37 +0000

    ThreadFilters - by

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -11,6 +11,7 @@ class ThreadFilters {
      * @var Request
      */
     protected $request;
+    protected $builder;
 
     public function __construct(Request $request)
     {
@@ -20,13 +21,24 @@ class ThreadFilters {
 
     public function apply($builder)
     {
+        $this->builder = $builder;
 
         if ($username = $this->request->by) {
-            $user = User::where('name', $username)->firstOrFail();
-            return $builder->where('user_id', $user->id);
+            return $this->by($username);
         }
 
         return $builder;
 
+    }
+
+    /**
+     * @param $builder
+     * @param $username
+     * @return mixed
+     */
+    protected function by($username)
+    {
+        $user = User::where('name', $username)->firstOrFail();
+        return $this->builder->where('user_id', $user->id);
     }
 }
