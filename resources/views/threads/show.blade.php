c4db36b955157521b06c59117a2a29910fc9ac3a
vagrant <vagrant@homestead>
2017-04-03 14:59:14 +0000

    summary thread

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-8">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h3>{{ $thread->title }}</h3>
@@ -14,24 +14,16 @@
                         {{ $thread->body }}
                     </div>
                 </div>
-            </div>
-        </div>
 
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
                 @foreach( $thread->replies as $reply)
                     @include('threads.reply')
                 @endforeach
-            </div>
-        </div>
 
-        @if( auth()->check() )
-            <div class="row">
-                <div class="col-md-8 col-md-offset-2">
+                @if( auth()->check() )
                     <form action="{{ $thread->path() . '/replies/' }}" method="POST">
-                        {{ csrf_field() }}
+                    {{ csrf_field() }}
 
-                        <!-- Body Form Input -->
+                    <!-- Body Form Input -->
                         <div class="form-group">
                             <textarea name="body" id="body" class="form-control">{{ old('body') }}</textarea>
                         </div>
@@ -40,14 +32,27 @@
                             <button type="submit" class="btn btn-primary">Post</button>
                         </div>
                     </form>
-                </div>
+                @else
+                    <p>
+                        Please
+                        <a href="{{ route('login') }}">sign in </a>
+                        to participate in this discussion
+                    </p>
+                @endif
+
             </div>
-        @else
-            <p>
-                Please
-                <a href="{{ route('login') }}">sign in </a>
-                to participate in this discussion
-            </p>
-        @endif
+
+            <div class="col-md-4">
+
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        This thread is created by {{ $thread->creator->name }}
+                        {{ $thread->created_at->diffForHumans() }}.
+                        It has {{ $thread->replies()->count() }} comments.
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
 @endsection
