755fdba3c6a279ab3a6d92974e7eaf55b64d8d52
vagrant <vagrant@homestead>
2017-04-05 16:49:46 +0000

    replies count on threads page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,19 @@
             'csrfToken' => csrf_token(),
         ]) !!};
     </script>
+
+    <style>
+        body { padding-bottom: 100px; }
+
+        .level {
+            display: flex;
+            align-items: center;
+        }
+
+        .flex {
+            flex: 1;
+        }
+    </style>
 </head>
 <body style="padding-bottom: 100px;">
 <div id="app">
