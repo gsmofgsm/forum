11eb2e45f96ab2d19be329c70bfe25214e34ee9c
vagrant <vagrant@homestead>
2017-04-05 16:47:03 +0000

    home pagina route

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ Route::get('/', function () {
 });
 
 
+Route::get('/', 'ThreadsController@index')->name('home');
+
 Route::get('/threads', 'ThreadsController@index');
 Route::get('/threads/create', 'ThreadsController@create');
 Route::get('/threads/{channel}', 'ThreadsController@index');
